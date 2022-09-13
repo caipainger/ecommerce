b013d1f486c55923ec64b32d85131a0a360ac41a
caipainger <caipaingenieriaprueba@gmail.com>
2022-09-12 21:07:19 -0500

    creacion del proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'template-ecommerce-5f073',
+    appId: '1:1060746912027:web:63da186dccac7b65a533be',
+    storageBucket: 'template-ecommerce-5f073.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCFceM0HPtLTL4e-W3klm-8R2xhIB2T_5U',
+    authDomain: 'template-ecommerce-5f073.firebaseapp.com',
+    messagingSenderId: '1060746912027',
+    measurementId: 'G-HK3L5QPFY9',
+  },
   production: false
 };
 
